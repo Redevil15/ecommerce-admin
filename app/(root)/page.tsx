5475fd790a9cba71446704d24d7b4b218c5f1db8
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2023-08-22 17:53:01 -0600

    Feat: Add shadcn element

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Hello admin dashboard</p>
-    </main>
+    <div>
+      <Button size="sm" variant="default">
+        Click Me
+      </Button>
+    </div>
   )
 }
