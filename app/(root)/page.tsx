9843bd52a6bf5d27d581b05dc48a44295cb02bbb
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2023-08-22 18:18:33 -0600

    Feat: Add CLERK autentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,9 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <Button size="sm" variant="default">
-        Click Me
-      </Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   )
 }
