4df3c8c16b2f43e4851ff37a35a9c03126a27094
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2023-08-31 12:58:22 -0600

    Feat: Add upload image through cloudinary

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+};
+
