e89c0caeed29d36f5acd761dd1c21a220615f430
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2023-08-30 22:18:05 -0600

    Feat: Setup settingsFormPage

---
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+
+
+interface SettingsFormProps {
+  initialData: Store;
+}
+
+export const SettingsForm: React.FC<SettingsFormProps> = ({
+  initialData
+}) => {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title="Settings"
+          description="Manage store preferences"
+        />
+        <Button
+          variant="destructive"
+          size="icon"
+          onClick={() => { }}
+        >
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+    </>
+  )
+};
